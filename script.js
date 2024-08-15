5cb2771c9c567234011643032e14df2c3c919e4c
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-15 12:03:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function fibonacci(num) {
-// your code here
-	if(num === 0) return 0;
-	if(num === 1) return 1;
-	let a = 0;
-	let b = 1;
-	let c;
-	for(let i = 2; i <= num; i++)
-		{
-			c = a + b;
-			a = b;
-			b = c;
-		}
-	return b;
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+
+    let a = 0;
+    let b = 1;
+    let c;
+
+    for (let i = 2; i <= num; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
 }
 
+console.log(fibonacci(1)); 
+console.log(fibonacci(5)); 
